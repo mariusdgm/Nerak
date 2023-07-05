8e188535b136a4ad2ba534758f5ae2abd6b92b88
mariusdgm <dragomir10m@yahoo.com>
2023-07-05 19:49:38 +0300

    fixed input style

--- src/chat_project/chat/forms.py
+++ src/chat_project/chat/forms.py
@@ -1,4 +1,12 @@
 from django import forms
 
+
 class MessageForm(forms.Form):
-    message_text = forms.CharField(max_length=1000, widget=forms.TextInput(attrs={'placeholder': 'Send a message', 'autocomplete': 'off'}))  
+    message_text = forms.CharField(max_length=1000, widget=forms.TextInput(attrs={'placeholder': 'Send a message', 'autocomplete': 'off'}))
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['message_text'].widget.attrs['oninput'] = 'updateSubmitButton(this)'
+
+    def is_submit_disabled(self):
+        return not self.is_bound or self.cleaned_data['message_text'].strip() == ''
