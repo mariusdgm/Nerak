634d156c511f2eea8591164aebb43c6e597aac7b
mariusdgm <dragomir10m@yahoo.com>
2023-07-10 13:20:16 +0300

    integrated local llm

--- src/chat_project/chat/views.py
+++ src/chat_project/chat/views.py
@@ -14,29 +14,18 @@ HUMAN_EMOTICON = "( ͡° ͜ʖ ͡°)"
 prompt_message = {"role": "user", "content": "You play the role of a sarcastic and grumpy person called Nerak. You always answer in a sarcastic manner and provide no useful responses. Make sure that you never answer with the same answer consecutively. Start the conversation by welcoming the user and introducing yourself."}
 chat_history.append(prompt_message)
 # returned_answer = gptj.chat_completion(chat_history, verbose=False, streaming=False)
-# chat_history.append(returned_answer["choices"][0]["message"])
-
-# displayed_messages = [
-#     {
-#         "sender": "bot",
-#         "emoticon": BOT_EMOTICON,
-#         "content": returned_answer["choices"][0]["message"]["content"],
-#         "timestamp": datetime.now().strftime("%Y-%m-%d %H:%M"),
-#     },
-# ]
+returned_answer = gptj.chat_completion([prompt_message], verbose=False, streaming=False)
+chat_history.append(returned_answer["choices"][0]["message"])
 
 displayed_messages = [
     {
         "sender": "bot",
         "emoticon": BOT_EMOTICON,
-        "content": "Hello",
+        "content": returned_answer["choices"][0]["message"]["content"],
         "timestamp": datetime.now().strftime("%Y-%m-%d %H:%M"),
     },
 ]
 
-def simulate_answer():
-    time.sleep(3)
-    return "Bot answer"
 def chat_page(request):
     form = MessageForm()
     if request.method == "POST":
@@ -49,7 +38,9 @@ def chat_page(request):
             chat_history.append(built_message)
 
             # returned_answer = gptj.chat_completion(chat_history, verbose=False, streaming=False)
-            # chat_history.append(returned_answer["choices"][0]["message"])
+            returned_answer = gptj.chat_completion([built_message], verbose=False, streaming=False)
+            
+            chat_history.append(returned_answer["choices"][0]["message"])
 
             displayed_messages.append(
                 {
@@ -60,31 +51,20 @@ def chat_page(request):
                 }
             )
 
-            # displayed_messages.append(
-            #     {
-            #         "sender": "bot",
-            #         "emoticon": BOT_EMOTICON,
-            #         "content": returned_answer["choices"][0]["message"]["content"],
-            #         "timestamp": datetime.now().strftime("%Y-%m-%d %H:%M"),
-            #     }
-            # )
-            res = simulate_answer()
-
             displayed_messages.append(
                 {
                     "sender": "bot",
                     "emoticon": BOT_EMOTICON,
-                    "content": res,
+                    "content": returned_answer["choices"][0]["message"]["content"],
                     "timestamp": datetime.now().strftime("%Y-%m-%d %H:%M"),
                 }
             )
-            
-            form.clear_input_field()  # Clear the input field
+
             form = MessageForm()
 
             response_data = {
                     "user_message": user_message_text,
-                    "bot_message": res,  # Replace with actual bot response
+                    "bot_message": returned_answer["choices"][0]["message"]["content"],  # Replace with actual bot response
                 }
             return JsonResponse(response_data)
 
